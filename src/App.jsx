2539fedee74e0c461ee493d7810664ce53a2bef9
Nitin-Goyal12 <your_email@example.com>
2025-11-16 21:36:19 +0530

    project ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/HomeP";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
